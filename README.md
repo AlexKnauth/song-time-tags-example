25c70ad79049f260ea6b87c7c51fb974285a576e
AlexKnauth <alexander@knauth.org>
2019-02-24 13:37:10 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# song-time-tags-example
+
+Examples of using `#lang song-time-tags` to plot recurring phrases/motifs in musicals.
+
---
+++ info.rkt
@@ -0,0 +1,10 @@
+#lang info
+
+(define collection 'multi)
+
+(define deps
+  '("base"
+    "sweet-exp-lib"
+    "git://github.com/AlexKnauth/song-time-tags"
+    ))
+

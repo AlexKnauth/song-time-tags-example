99dd75e3006ae0362a6458db4ffe12e134453f25
AlexKnauth <alexander@knauth.org>
2019-02-26 23:09:30 -0500

    start on les-miz

---
+++ song-time-tags-example/les-misérables.rkt
@@ -0,0 +1,65 @@
+#lang song-time-tags
+
+dimensions 1400 720
+
+;; ------------------------------------------------------------------------
+
+song-time-tags
+  (1, 1)
+    0:00 LD
+    0:12 LD
+    0:30 LD
+    0:42 LD
+    0:53 LD
+    1:05 LD
+    1:16 LD
+    1:27 LD
+    1:38 LD
+    1:50 LD
+    2:06 LD
+    2:18 LD
+    2:30 N24601
+    3:25 N24601
+    4:29 FYLSE
+    ;4:54 may God's blessing
+    5:00 I-just-know-this-is-important
+    5:23 I-just-know-this-is-important
+  (1, 2)
+    0:45 chained-me-and-left-me-for-dead
+    0:58 something-teach-me-love
+    1:38 take-an-eye-for-an-eye
+    1:54 something-in-the-instrumental-same-as-One-Day-More
+    2:03 something-teach-me-love
+    2:40 I-am-reaching
+  (1, 3)
+    0:00 waterfall
+
+;; ------------------------------------------------------------------------
+
+song-names
+  (1, 1) "Prologue"
+  (1, 2) "Valjean's Soliloquy (What Have I Done?)"
+  (1, 3) "At the End of the Day"
+
+;; ------------------------------------------------------------------------
+
+song-durations
+  (1, 1) 10:06
+  (1, 2) 3:22
+  (1, 3) 5:09
+
+;; ------------------------------------------------------------------------
+
+;; NOTE:
+;; For it to play the clips when you click the dots on the plot, you
+;;   must have the "Complete Symphonic Recording" of Les Misérables in
+;;   your Itunes Music folder.
+;; There could be other ways of looking up the files in the future
+;;   (other than Itunes), but the current implementation does rely on
+;;   it being a file on your computer.
+
+song-files
+  itunes
+    #:album-artist "Les Misérables: International Cast"
+    #:album "Les Misérables: The Complete Symphonic Recording"
+
